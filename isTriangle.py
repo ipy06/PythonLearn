c75719e944e49b5e1a0d34a34472178db8f40b08
ipy06 <Ipinoluwaheze@gmail.com>
2024-05-13 20:45:19 -0500

    made interactive additions

--- isTriangle.py
+++ isTriangle.py
@@ -1,5 +1,5 @@
 """
-This is a program to determine if given coordinates form a valid triangle.
+This is an interactive program to determine if given coordinates form a valid triangle.
 
 """
 
@@ -7,8 +7,27 @@ This is a program to determine if given coordinates form a valid triangle.
 def is_a_triangle(a, b, c):
     return a + b > c and b + c > a and c + a > b
 
+def is_a_right_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return False
+    if c > a and c > b:
+        return c ** 2 == a ** 2 + b ** 2
+    if a > b and a > c:
+        return a ** 2 == b ** 2 + c ** 2
 
-print(is_a_triangle(1, 1, 1))
-print(is_a_triangle(1, 1, 3))
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it is a right-angled triangle.')
+else:
+    print('No, it is not a right-angled triangle.')
+
+# print(is_a_right_triangle(1, 1, 1))
+# print(is_a_right_triangle(1, 1, 3))
+# print(is_a_right_triangle(5, 3, 4))
+# print(is_a_right_triangle(1, 3, 4))
 
 
